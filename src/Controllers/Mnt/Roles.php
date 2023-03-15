81e6fb4930a1c241d05aeed80717dff54fc81375
unknown <emerson.zapatarivas@gmial.com>
2023-03-15 15:30:19 -0600

    Solucion de error de Edicion en Roles

--- src/Controllers/Mnt/Roles.php
+++ src/Controllers/Mnt/Roles.php
@@ -1,19 +1,20 @@
 <?php
+
 namespace Controllers\Mnt;
 
 use Controllers\PublicController;
 use Views\Renderer;
 
-class Roles extends PublicController {
-    public function run() :void
-    {
-        $viewData = array(
-            "edit_enabled"=>true,
-            "delete_enabled"=>true,
-            "new_enabled"=>true
-        );
-        $viewData["roles"] = \Dao\Mnt\Roles::findAll();
-        Renderer::render('mnt/roles', $viewData);
-    }
+class Roles extends PublicController
+{
+  public function run(): void
+  {
+    $viewData = array(
+      "edit_enabled" => true,
+      "delete_enabled" => true,
+      "new_enabled" => true
+    );
+    $viewData["roles"] = \Dao\Mnt\Roles::findAll();
+    Renderer::render('mnt/roles', $viewData);
+  }
 }
-?>
